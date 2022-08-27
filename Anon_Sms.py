fd7788f3d2a9d5a743c560d467626a3086fe7388
Doc. Shaka <61460961+DocShaka@users.noreply.github.com>
2022-08-27 16:30:00 +0200

    Create Anon_Sms.py

---
+++ Anon_Sms.py
@@ -0,0 +1,117 @@
+import os
+from os import *
+from art import *
+from termcolor import colored
+from colorama import Back, Fore, Style, deinit, init
+from tqdm import tqdm
+import socket
+import faker
+from faker import Faker
+import webbrowser
+
+
+# Ascii art
+
+print(colored(text2art ("\t___________________________"),'red'))
+print(colored(text2art ("\t________ANON_SMS___________"),'cyan'))
+#print(colored(text2art ("\t___________________________\n"),'blue'))
+#print(colored(text2art ("\t______Sms__anomyme________"),'cyan'))
+#print(colored(text2art ("\t___________________________\n"),'red'))
+#print(colored(text2art ("\t______Coded By: DocShaka ______"),'yellow'))
+#print(colored(text2art ("\t_____github.com/DocShaka_____"),'green'))
+print(colored(text2art ("\t___________________________"),'red'))
+
+# Code pure
+print ()
+print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Je check la configuration des dépendences. ")
+print (Fore.MAGENTA + Style.NORMAL + "")
+for i in tqdm(range(100000)): 
+        pass
+def is_connected():
+    try:
+        fake = Faker(locale="fr_FR")
+        socket.create_connection(("www.google.com", 80))
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.YELLOW + Style.NORMAL + "[ ✔ ] Internet.............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.RED + Style.NORMAL + " [ X ] Curl.............[ FALSE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + "Je télécharge Curl  pour vous. ")
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(100000000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " J'installe Curl pour vous. ")
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(1000000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ",Fore.YELLOW + Style.NORMAL + "[ ✔ ] Curl.............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ",Fore.RED + Style.NORMAL + "[ X ] Git.............[ FALSE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + "Je télécharge Git  pour vous. ")
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(100000000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + "J'installe Git  pour vous. ")
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(1000000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.YELLOW + Style.NORMAL + "[ ✔ ] Git.............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.YELLOW + Style.NORMAL + "[ ✔ ] Toute les dépendances.............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Entrez le code du pays [ Ex: +33 (France) ] ")
+        print ()
+        code = input(Fore.BLUE + Style.NORMAL + "\tAnon_Sms:> ")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Entrez le numéro de téléphone [ Ex: 654695874 ( sans le premier chiffre ) ] ")
+        print ()
+        numero = input(Fore.BLUE + Style.NORMAL + "\tAnon_Sms:> ") 
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Vous avez entré ", code, numero, " , Nous vérifions sa validité. ")
+        print ()
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(10000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.YELLOW + Style.NORMAL + "[ ✔ ] Validité du ", code, numero, ".............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Entrez le message à envoyer.")
+        print ()
+        message = input(Fore.BLUE + Style.NORMAL + "\tAnon_Sms:> ") 
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Vous allez envoyez aux", "[", code, numero ,"] le message ", "[",  message, "] .")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " je génére le numéro temporaire pour votre utilisation.")
+        print ()
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(10000)): 
+            pass
+        print ()
+        faux_numero = fake.phone_number()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.YELLOW + Style.NORMAL + "[ ✔ ] Génération du numéro temporaire.............[ TRUE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " J'ai généré le numéro temporaire suivant", "[", faux_numero, "] pour votre utilisation.")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " j'envoie aux", "[", code, "]", "[", numero ,"] le message ", "[",  message, "] avec le numéro temporaire [" ,faux_numero ,"].")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Veuillez patientez , SVP.")
+        print (Fore.MAGENTA + Style.NORMAL + "")
+        for i in tqdm(range(10000)): 
+            pass
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Votre message as bien été envoyé.")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + "Merci d 'avoir utilisé Anon SMS , à trés bientôt.")
+        webbrowser.open('https://www.youtube.com/watch?v=SjHUb7NSrNk&ab_channel=Grayhyaena')
+        print ()
+    except OSError:
+        pass
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ",Fore.RED + Style.NORMAL + "[ X ] Internet.............[ FALSE ]")
+        print ()
+        print (Fore.BLUE + Style.NORMAL + "Anon_Sms:> ", Fore.WHITE + Style.NORMAL + " Il vous faut un accés internet , à trés bienôt !!! ")
+is_connected()
